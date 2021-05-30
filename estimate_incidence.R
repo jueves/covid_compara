ec035574364c6082c3cc00f7e8ec92e4b7cd7547
jueves <lcobiella@uoc.edu>
2021-05-30 21:15:03 +0100

    Add estimation of active cases.

---
+++ estimate_incidence.R
@@ -0,0 +1,52 @@
+library(tidyverse)
+library(lubridate)
+data_asign <- read.csv("data/cv19_asignacion.csv")
+data_resi <- read.csv("data/cv19_residencia.csv")
+
+data <- data_asign
+
+data$fecha_datos <- dmy(data$fecha_datos)
+data$fecha_caso <- dmy(data$fecha_caso)
+data$fecha_fallecido <- dmy(data$fecha_fallecido)
+data$fecha_curado <- dmy(data$fecha_curado)
+
+# Create date/municipio counter
+number_of_days = 150
+days_template <- seq(as.Date("2021/01/01"), by = "day", length.out = number_of_days)
+days_list <- c()
+municipios_list <- c()
+for (municipio in unique(data$municipio)){
+  days_list <- append(days_list, days_template)
+  municipios_list <- append(municipios_list, rep(municipio, number_of_days))
+}
+
+counter_df <- data.frame(fecha=days_list, municipio=municipios_list, cv19_activos=0)
+
+# Create a single end date for each case
+end_date <- c()
+for (i in 1:nrow(data)){
+  posible_dates <- as.Date(c(data$fecha_fallecido[i], data$fecha_curado[i], today()+1),
+                           origin="1970-01-01")
+  end_date <- append(end_date, min(posible_dates, na.rm=TRUE))
+}
+
+data['fecha_final'] <- end_date
+
+municipio_list <- c()
+fecha_list <- c()
+for (i in 1:nrow(data)){
+  num_days <- as.numeric(data$fecha_final[i] - data$fecha_caso[i])
+  if (num_days < 0){
+    num_days <- 0
+  }
+  case_dates <- seq(data$fecha_caso[i], by = "day", length.out = num_days)
+  case_municipio <- rep(as.character(data$municipio[i]), num_days)
+  
+  municipio_list <- append(municipio_list, case_municipio)
+  fecha_list <- append(fecha_list, case_dates)
+}
+base_data <- data.frame(fecha=fecha_list, municipio=municipio_list)
+base_data %>%
+  group_by(fecha, municipio) %>%
+  count() -> counter_df
+
